7d6ac353f98bd7f5961f369dd14bc7432efbccb0
miahro <mikko.ahro@gmail.com>
2022-11-28 12:27:27 +0200

    updated db configuration code

--- schooltasks/src/config.py
+++ schooltasks/src/config.py
@@ -1,7 +1,7 @@
 """konfigurointimoduuli asettaa tiedostopolut
 tiedostojen nimet luetaan .env tiedostosta"""
 import os
-from dotenv import load_dotenv
+from dotenv import load_dotenv  # pylint: disable=import-error
 
 directory = os.path.dirname(__file__)
 
@@ -12,7 +12,8 @@ TASKS_INPUT_FILENAME = os.getenv("TASKS_INPUT_FILE")
 TASKS_INPUT_PATH = os.path.join(directory, "..", "data", TASKS_INPUT_FILENAME)
 
 TOPICS_INPUT_FILENAME = os.getenv("TOPICS_INPUT_FILE")
-TOPICS_INPUT_PATH = os.path.join(directory, "..", "data", TOPICS_INPUT_FILENAME)
+TOPICS_INPUT_PATH = os.path.join(
+    directory, "..", "data", TOPICS_INPUT_FILENAME)
 
 DB_FILE = os.getenv("DB_FILE")
 DB_FILE_PATH = os.path.join(directory, "..", "data", DB_FILE)
--- schooltasks/src/dbinit.py
+++ schooltasks/src/dbinit.py
@@ -1,7 +1,11 @@
+"""tietokannan alustusfunktiot"""
+
 from dbcon import db_connection
 
-def create_tables(connection):
 
+def create_tables(connection):
+    """luo tietokantataulut
+    Args connection: tietokantayhteys"""
     cursor = connection.cursor()
     cursor.execute("""CREATE TABLE Users(
             id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -10,13 +14,13 @@ def create_tables(connection):
             user_id TEXT NOT NULL UNIQUE,
             passwd TEXT NOT NULL,
             teacher BOOLEAN);"""
-           )
+                   )
     connection.commit()
 
     cursor.execute("""CREATE TABLE Topics (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
             topic TEXT NOT NULL UNIQUE);"""
-           )
+                   )
     connection.commit()
 
     cursor.execute("""CREATE TABLE Tasks (
@@ -28,7 +32,7 @@ def create_tables(connection):
             wrong1 TEXT NOT NULL,
             wrong2 TEXT NOT NULL,
             wrong3 TEXT NOT NULL);"""
-    )
+                   )
 
     connection.commit()
 
@@ -37,29 +41,38 @@ def create_tables(connection):
             person_id INTEGER REFERENCES Users,
             task_id INTEGER REFERENCES Tasks,
             result BOOLEAN);"""
-    )
+                   )
     connection.commit()
 
+
 def delete_tables(connection):
+    """tuhoaa kaikki tietokantataulut
+    Args connection: tietokantayhteys"""
     cursor = connection.cursor()
 
     cursor.execute("DROP TABLE IF EXISTS Users")
-    connection.commit
+    connection.commit()
 
     cursor.execute("DROP TABLE IF EXISTS Topics")
-    connection.commit
+    connection.commit()
 
     cursor.execute("DROP TABLE IF EXISTS Tasks")
-    connection.commit
-    
+    connection.commit()
+
     cursor.execute("DROP TABLE IF EXISTS Results")
-    connection.commit
- 
+    connection.commit()
+
 
 def init_db():
+    """alustaa tietokannan
+    hakee tietokantayhteyden db_conn modulista
+    tuhoaa kaikki taulut
+    ja luo kaikki taulut """
+
     connection = db_connection()
     delete_tables(connection)
     create_tables(connection)
+
 
 if __name__ == "__main__":
     init_db()
