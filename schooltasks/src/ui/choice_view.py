0af4adbffc89b48a86b3510605a0c819f5a1d11d
miahro <mikko.ahro@gmail.com>
2022-12-05 10:41:24 +0200

    choice view in UI expanded

--- schooltasks/src/ui/choice_view.py
+++ schooltasks/src/ui/choice_view.py
@@ -1,5 +1,7 @@
-from tkinter import ttk, constants
+from tkinter import ttk, constants, OptionMenu, StringVar
 from services.user_services import userservices
+from services.topic_services import topicservices
+from services.task_services import taskservices
 
 
 class ChoiceView:
@@ -9,6 +11,7 @@ class ChoiceView:
         self._handle_task = handle_task
         self._handle_result = handle_result
         self._show_start_view = show_start_view
+#        self._choice_text = None
         self._frame = None
 
         self._initialize()
@@ -23,34 +26,86 @@ class ChoiceView:
         userservices.logout()
         self._show_start_view()
 
+    def _topic_choice(self, choice):
+        topicservices.set_active_topic(choice)
+       # print(choice)
+        self.destroy()
+        self._initialize()
+
+
+    def _difficulty_choice(self, choice):
+        taskservices.set_active_difficulty(choice)
+        self.destroy()
+        self._initialize()
+
+
     def _initialize(self):
         self._frame = ttk.Frame(master=self._root)
-        label = ttk.Label(master=self._frame, text="Valintanäkymä")
-        label.grid(row=5, column=0)
+        label = ttk.Label(master=self._frame, text="Tehtävien valinta", font=(
+            'Helvetica', 12, 'bold'))
+        label.grid(padx=5, pady=5, sticky=constants.EW)
 
         user_label = ttk.Label(
             master=self._frame, text=f"Kirjautunut käyttäjä: {userservices.active_user_details()['user_id']}")
-        user_label.grid(row=6, column=0)
+        user_label.grid(padx=5, pady=5, sticky=constants.EW)
 
-        button = ttk.Button(
+        logout_button = ttk.Button(
             master=self._frame,
             text="Kirjaudu ulos",
             command=self._logout
         )
-        button.grid(row=8, column=0)
+        logout_button.grid(padx=5, pady=5, sticky=constants.EW)
 
-        button = ttk.Button(
-            master=self._frame,
-            text="Tee tehtäviä",
-            command=self._handle_task
-        )
-        button.grid(row=12, column=0)
+#aiheen valinta
+        top_choice_label = ttk.Label(
+            master=self._frame, text="Valitse aihe", font=('Helvetica', 12, 'bold'))
+        top_choice_label.grid(padx=5, pady=5, sticky=constants.EW)
 
-        button = ttk.Button(
+        topic_label = ttk.Label(
+            master=self._frame, text=f"Valittu aihe: {topicservices.return_active_topic()}")
+        topic_label.grid(padx=5, pady=5, sticky=constants.EW)
+
+        topics = topicservices.get_all_topics()
+        topic_var = StringVar()
+        topic_var.set(topics[0])  # default value
+
+        topic_options = OptionMenu(
+            self._frame, topic_var, *topics, command=self._topic_choice)
+        topic_options.grid(padx=5, pady=5, sticky=constants.EW)
+
+#aiheen valinta
+
+        level_choice_label = ttk.Label(
+            master=self._frame, text="Valitse vaikeustaso", font=('Helvetica', 12, 'bold'))
+        level_choice_label.grid(padx=5, pady=5, sticky=constants.EW)
+
+        difficulty_label = ttk.Label(
+            master=self._frame, text=f"Valittu vaikeustaso: {taskservices.return_active_difficulty_str()}")
+        difficulty_label.grid(padx=5, pady=5, sticky=constants.EW)
+
+
+        difficulty = taskservices.return_difficulty_range()
+        difficulty_var = StringVar()
+        difficulty_var.set(difficulty[0])  # default value
+
+
+        difficulty_options = OptionMenu(
+            self._frame, difficulty_var, *difficulty, command=self._difficulty_choice)
+        difficulty_options.grid(padx=5, pady=5, sticky=constants.EW)
+
+        if topicservices.active_topic is not None and taskservices.active_difficulty is not None:
+            start_task_button = ttk.Button(
+                master=self._frame,
+                text="Tee tehtäviä",
+                command=self._handle_task
+            )
+            start_task_button.grid(padx=5, pady=5, sticky=constants.EW)
+
+        result_button = ttk.Button(
             master=self._frame,
             text="Näytä tulokset",
             command=self._handle_result
         )
-        button.grid(row=15, column=0)
+        result_button.grid(padx=5, pady=5, sticky=constants.EW)
 
         self.pack()
