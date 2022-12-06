1eef2f36700cea05a2f5fdec3f030109e671709b
miahro <mikko.ahro@gmail.com>
2022-12-06 12:02:08 +0200

    preliminary results view improved

--- schooltasks/src/ui/results_view.py
+++ schooltasks/src/ui/results_view.py
@@ -1,4 +1,4 @@
-from tkinter import ttk, constants, Text
+from tkinter import ttk, constants, Text, Entry
 from services.user_services import userservices
 from services.result_services import resultservices
 
@@ -23,13 +23,30 @@ class ResultView:
             master=self._frame, text=f"Oppilaan {userservices.active_user_details()['first_name']} {userservices.active_user_details()['last_name']} tulokset: "
         )
         user_label.grid(padx=5, pady=5, sticky=constants.EW)
-        totals_label = ttk.Label(
-            master=self._frame, text=f"Tehtäviä yhteensä: {totals['total_tasks']} \n"
-            f"oikein: {totals['correct']}\n"
-            f"väärin: {totals['fail']}\n"
-            f"oikein prosentteina: {totals['correct_percent']}"
-        )
-        totals_label.grid(padx=5, pady=5, sticky=constants.EW)
+        # totals_label = ttk.Label(
+        #     master=self._frame, text=f"Tehtäviä yhteensä: {totals['total_tasks']} \n"
+        #     f"oikein: {totals['correct']}\n"
+        #     f"väärin: {totals['fail']}\n"
+        #     f"oikein prosentteina: {totals['correct_percent']}"
+        # )
+        # totals_label.grid(padx=5, pady=5, sticky=constants.EW)
+
+        columns = ("difficulty", "total", "correct", "fail", "correct_percent")
+        
+        tree= ttk.Treeview(master=self._frame, columns=columns ,height = 40)
+        tree.grid(padx = 5, pady = 5)
+
+
+        tree.heading('#0', text='Aihe')
+        tree.heading('difficulty', text='Vaikeustaso')
+        tree.heading('total', text='Yhteensä')
+        tree.heading('correct', text='Oikein')
+        tree.heading('fail', text='Väärin')
+        tree.heading('correct_percent', text='Oikein-%')
+
+        tree.insert('', 'end', iid=0, text="kaikki aiheet", values=(f"kaikki vaikeustasot",f"{totals['total_tasks']}", 
+                    f"{totals['correct']}", f"{totals['fail']}", f"{totals['correct_percent']} %" ))
+
 
     def _initialize(self):
         self._frame = ttk.Frame(master=self._root)
@@ -38,7 +55,7 @@ class ResultView:
 
         user_label = ttk.Label(
             master=self._frame, text=f"Kirjautunut käyttäjä: {userservices.active_user_details()['user_id']}")
-        user_label.grid(row=8, column=0)
+        user_label.grid(padx=5, pady=5, sticky=constants.EW)
 
         result_label = ttk.Label(
             master=self._frame, text=f"Tulokset", font=('Helvetica', 12, 'bold'))
