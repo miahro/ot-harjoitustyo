9f48449f377b22f03c22408644da1d4aeeba7026
miahro <mikko.ahro@gmail.com>
2022-12-19 15:01:09 +0200

    error handling added to build.py

--- schooltasks/src/build.py
+++ schooltasks/src/build.py
@@ -1,10 +1,16 @@
 
 """moduli mahdollistaa tietokannan luonnin komentoriviltä"""
+from os.path import isfile
+from config import TASKS_INPUT_PATH
 from dbinit import init_db
 
 
 def build():
     """kutsuu vain dbinit.init_db()-funktiota"""
+    if not isfile(TASKS_INPUT_PATH):
+        print("Error: questions list missing")
+        print("run poetry run invoke generate-questions or manually generate questions")
+        exit()
     init_db()
 
 
