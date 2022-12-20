677279a5d56d01e3268bce150f7b79c41e8bd16b
miahro <mikko.ahro@gmail.com>
2022-12-20 10:31:02 +0200

    added DB not initialized error treatment

--- schooltasks/src/index.py
+++ schooltasks/src/index.py
@@ -1,5 +1,7 @@
 """moduli sisältää pääohjelman"""
+import sys
 from tkinter import Tk
+from dbcon import connection
 from ui.ui import UI
 
 
@@ -17,4 +19,22 @@ def main():
 
 
 if __name__ == "__main__":
+    #komentoriviltä suoritettaessa tarkistetaan tietokannan taulujen
+    #olemassaolo
+    if not connection.execute(
+        "SELECT COUNT(*) FROM sqlite_master\
+        WHERE type='table' AND name='Results';").fetchone()[0] == 1 or\
+    not connection.execute(
+        "SELECT COUNT(*) FROM sqlite_master\
+        WHERE type='table' AND name='Users';").fetchone()[0] == 1 or\
+    not connection.execute(
+        "SELECT COUNT(*) FROM sqlite_master\
+        WHERE type='table' AND name='Topics';").fetchone()[0] == 1 or\
+    not connection.execute(
+        "SELECT COUNT(*) FROM sqlite_master\
+        WHERE type='table' AND name='Tasks';").fetchone()[0] == 1:
+        print("Database not initialized")
+        print("Run build before starting program")
+        sys.exit("DB not initialized")
+
     main()
