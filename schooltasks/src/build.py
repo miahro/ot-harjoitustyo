adb2d38912331636162931bfca7ec0d10e47af7f
miahro <mikko.ahro@gmail.com>
2022-12-20 10:28:46 +0200

    added file missing treatment to init files

--- schooltasks/src/build.py
+++ schooltasks/src/build.py
@@ -1,5 +1,6 @@
 
 """moduli mahdollistaa tietokannan luonnin komentoriviltä"""
+import sys
 from os.path import isfile
 from config import TASKS_INPUT_PATH
 from dbinit import init_db
@@ -7,12 +8,12 @@ from dbinit import init_db
 
 def build():
     """kutsuu vain dbinit.init_db()-funktiota"""
+    init_db()
+
+if __name__ == "__main__":
+    #komentoriviltä suoritettaessa tarkistetaan tehtävätiedoston olemassaolo
     if not isfile(TASKS_INPUT_PATH):
         print("Error: questions list missing")
         print("run poetry run invoke generate-questions or manually generate questions")
-        exit()
-    init_db()
-
-
-if __name__ == "__main__":
+        sys.exit("Questions list missing")
     build()
--- schooltasks/src/config.py
+++ schooltasks/src/config.py
@@ -1,7 +1,7 @@
 """konfigurointimoduuli asettaa tiedostopolut
 tiedostojen nimet luetaan .env tiedostosta"""
 import os
-from dotenv import load_dotenv  # pylint: disable=import-error
+from dotenv import load_dotenv
 
 directory = os.path.dirname(__file__)
 
@@ -21,4 +21,4 @@ DB_FILE_PATH = os.path.join(directory, "..", "data", DB_FILE)
 TASK_KEYS = ("topic_id", "difficulty", "question",
              "correct", "wrong1", "wrong2", "wrong3", "task_id")
 
-DIFFICULTY_RANGE = list(range(1,11))
+DIFFICULTY_RANGE = list(range(1, 11))
