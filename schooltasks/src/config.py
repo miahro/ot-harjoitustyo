90c6f3a3fca0354d5eb98357a5fc404eee4b0348
miahro <mikko.ahro@gmail.com>
2022-12-20 11:39:45 +0200

    added defaults for file names

--- schooltasks/src/config.py
+++ schooltasks/src/config.py
@@ -8,14 +8,14 @@ directory = os.path.dirname(__file__)
 load_dotenv(dotenv_path=os.path.join(directory, "..", ".env"))
 
 
-TASKS_INPUT_FILENAME = os.getenv("TASKS_INPUT_FILE")
+TASKS_INPUT_FILENAME = os.getenv("TASKS_INPUT_FILE") or "task.csv"
 TASKS_INPUT_PATH = os.path.join(directory, "..", "data", TASKS_INPUT_FILENAME)
 
-TOPICS_INPUT_FILENAME = os.getenv("TOPICS_INPUT_FILE")
+TOPICS_INPUT_FILENAME = os.getenv("TOPICS_INPUT_FILE") or "topic.csv"
 TOPICS_INPUT_PATH = os.path.join(
     directory, "..", "data", TOPICS_INPUT_FILENAME)
 
-DB_FILE = os.getenv("DB_FILE")
+DB_FILE = os.getenv("DB_FILE") or "db.sqlite3"
 DB_FILE_PATH = os.path.join(directory, "..", "data", DB_FILE)
 
 TASK_KEYS = ("topic_id", "difficulty", "question",
